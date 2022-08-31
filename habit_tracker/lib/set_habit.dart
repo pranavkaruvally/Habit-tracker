80b5cafa9abc02cd87ed44fd1186f95223ca6353
Your Name <pranavsdreams@gmail.com>
2022-08-31 15:35:07 +0530

    Made calendar widget

--- habit_tracker/lib/set_habit.dart
+++ habit_tracker/lib/set_habit.dart
@@ -1,5 +1,57 @@
 import 'package:flutter/material.dart';
 
+Widget calendarTile({date, weekDay}) {
+  return Container(
+          margin: const EdgeInsets.all(10.0),
+          height: 80,
+          width: 80,
+          child: Card(
+            shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10.0),),
+            color: const Color(0xFF131a27),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Expanded(child: Container()),
+                  Container(margin: const EdgeInsets.all(5.0), child: Text("$date", style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold))),
+                  Container(margin: const EdgeInsets.all(5.0), child: Text("$weekDay", style: const TextStyle(color: Colors.white, fontSize: 15))),
+                  Expanded(child: Container()),
+                ],
+              ),
+            ),
+          ),
+        );
+}
+
+int date(int index) {
+  final thisDay = DateTime.now();
+  final concernedDay = thisDay.add(Duration(hours:24*index));
+  return concernedDay.day;
+}
+
+String week(int index) {
+  const List<String> weeks = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  final thisDay = DateTime.now();
+  final concernedDay = thisDay.add(Duration(hours:24*index));
+  return weeks[concernedDay.weekday-1];
+}
+
+Widget calendar() {
+  return Container(
+    height: 95,
+    color: const Color(0xFF1b232e),
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: 7,
+      itemBuilder: (context, index) => calendarTile(date: date(index), weekDay: week(index)),
+      // children: <Widget>[
+      //   calenderTile(date:2, weekDay:'Tue'),
+      //   calenderTile(date:3, weekDay:'Wed'),
+      //           ],
+              ),);
+}
+
+
 class SetHabit extends StatefulWidget {
   const SetHabit({Key? key}) : super(key: key);
 
@@ -28,7 +80,7 @@ class _SetHabitState extends State<SetHabit> {
         ),
         const SizedBox(width: 10.0,)],
       ),
-      //body: Center(child: Text("Hello World", style: TextStyle(color: Colors.white))),
+      body: Center(child: calendar()),
     );
   }
 }
