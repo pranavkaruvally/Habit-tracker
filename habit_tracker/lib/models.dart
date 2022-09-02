2cf535683e5d82bb40eb71a07ff4b37e0dff428f
Your Name <pranavsdreams@gmail.com>
2022-09-02 19:09:40 +0530

    Prepared database for storing habits

---
+++ habit_tracker/lib/models.dart
@@ -0,0 +1,37 @@
+import 'package:hive/hive.dart';
+import 'dart:math';
+
+part 'models.g.dart';
+
+@HiveType(typeId: 1)
+class Habit extends HiveObject {
+  @HiveField(0)
+  String? habit;
+
+  @HiveField(1, defaultValue: 0)
+  int? score;
+
+  @HiveField(2)
+  Map<DateTime, bool>? dates;
+
+  @HiveField(3)
+  int? color;
+
+  int _setColor() {
+    final random = Random();
+    List<int> colors = [0xFF7424fe, 0xFF2096f2, 0xFFf54337, 0xFF00bcd4];
+    int? i;
+
+    i = random.nextInt(colors.length);
+    return colors[i];
+  }
+
+  void setHabit(habit) {
+    DateTime now = DateTime.now();
+    DateTime date = DateTime(now.year, now.month, now.day);
+
+    this.habit = habit;
+    dates = {date: false};
+    color = _setColor();
+  }
+}
---
+++ habit_tracker/lib/models.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'models.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HabitAdapter extends TypeAdapter<Habit> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Habit read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Habit()
+      ..habit = fields[0] as String?
+      ..score = fields[1] == null ? 0 : fields[1] as int?
+      ..dates = (fields[2] as Map?)?.cast<DateTime, bool>()
+      ..color = fields[3] as int?;
+  }
+
+  @override
+  void write(BinaryWriter writer, Habit obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.habit)
+      ..writeByte(1)
+      ..write(obj.score)
+      ..writeByte(2)
+      ..write(obj.dates)
+      ..writeByte(3)
+      ..write(obj.color);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HabitAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
